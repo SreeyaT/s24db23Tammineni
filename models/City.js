99a9072177c4c9fcc6a44777768d244f731f65c5
SreeyaT <saisreeya1601@gmail.com>
2024-04-17 12:40:02 -0500

    rtdfg

--- models/City.js
+++ models/City.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose")
 const CitySchema = mongoose.Schema({
-    name: String,
-    country :String,
-    population :Number
+    name: {
+        type: String,
+        minlength: 1,
+        maxlength: 10,
+    },
+    country: {
+        type: String,
+        minlength: 1,
+        maxlength: 20,
+    },
+    population: Number
 })
 module.exports = mongoose.model("City",
-CitySchema)
+    CitySchema)
--- routes/city.js
+++ routes/city.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var passport = require('passport')
 const City_controllers = require('../controllers/city')
 var router = express.Router();
- 
+//A little function to check if we have an authorized user and continue on
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 /* GET home page. */
 router.get('/', City_controllers.City_view_all_Page);
 
@@ -9,12 +18,16 @@ router.get('/', City_controllers.City_view_all_Page);
 router.get('/detail', City_controllers.City_view_one_Page);
 
 /* GET create costume page */
-router.get('/create', City_controllers.City_create_Page);
+router.get('/create', secured, City_controllers.City_create_Page);
 
 /* GET create update page */
-router.get('/update', City_controllers.City_update_Page);
+router.get('/update', secured, City_controllers.City_update_Page);
+
+router.post('/login', passport.authenticate('local'), function (req, res) {
+    res.redirect('/');
+});
 
 /* GET delete costume page */
-router.get('/delete', City_controllers.City_delete_Page)
- 
+router.get('/delete', secured, City_controllers.City_delete_Page)
+
 module.exports = router;
